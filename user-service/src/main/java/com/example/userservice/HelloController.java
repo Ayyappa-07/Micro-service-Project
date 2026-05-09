90e198c92b143fe6936dcc0837b3b9ae84ef25eb
Ayyappa H N <ayyappahn11@gmail.com>
2026-05-09 19:22:29 +0530

    Remove HelloController class and its methods

--- user-service/src/main/java/com/example/userservice/HelloController.java
+++ user-service/src/main/java/com/example/userservice/HelloController.java
@@ -1,13 +1,1 @@
-package com.example.userservice;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class HelloController {
-
-    @GetMapping("/")
-    public String home() {
-        return "User Service Running Successfully!";
-    }
-}
